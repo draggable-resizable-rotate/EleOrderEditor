6c04bf4f9b2d17a1959e2b005534ea4a447623fb
shepijcanwu <shepijcanwu@tencent.com>
2022-06-15 10:34:38 +0800

    feat: 去除漏洞依赖

--- config/utils/portfinder.js
+++ config/utils/portfinder.js
@@ -1,19 +1,6 @@
-/* eslint-disable import/no-extraneous-dependencies */
-const portfinder = require('portfinder');
-
-portfinder.highestPort = 65535;
+// eslint-disable-next-line import/no-extraneous-dependencies
+const detect = require('detect-port');
 
 module.exports = function getEffectivePort(expectPort) {
-  // default: 8000
-  portfinder.basePort = parseInt(expectPort, 10);
-  return new Promise((resolve, reject) => {
-    // 查找端口号
-    portfinder.getPort((err, port) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve(port);
-    });
-  });
+  return detect(parseInt(expectPort, 10));
 };
