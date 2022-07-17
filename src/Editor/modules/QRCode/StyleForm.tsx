9f1f7a6542e01edfd2d43ab2308217e2678ab1aa
shepijcanwu <shepijcanwu@tencent.com>
2022-07-17 12:44:03 +0800

    feat: 接入QRCode的StyleForm

--- src/Editor/modules/QRCode/StyleForm.tsx
+++ src/Editor/modules/QRCode/StyleForm.tsx
@@ -4,15 +4,19 @@ import { Form } from 'antd';
 import PositionForm from '@/Editor/components/PositionForm';
 import { FormItemLayout } from '@/Editor/config';
 import { QRCodeModuleData } from './moduleClass';
-import { GroupModuleType } from '../TypeConstraints';
+import { GroupModuleType, StyleFormValue } from '../TypeConstraints';
 
 export interface QRCodeStyleFormProps {
   mergeModuleDataProps: QRCodeModuleData['props'];
+  onChange: (changeValues: StyleFormValue) => void;
 }
 
 const moduleType = GroupModuleType.QRCode;
 
-const QRCodeStyleForm: React.FC<QRCodeStyleFormProps> = ({ mergeModuleDataProps: mergeProps }) => {
+const QRCodeStyleForm: React.FC<QRCodeStyleFormProps> = ({
+  mergeModuleDataProps: mergeProps,
+  onChange,
+}) => {
   const [form] = Form.useForm();
 
   const { left, top, width, height } = mergeProps;
@@ -22,7 +26,7 @@ const QRCodeStyleForm: React.FC<QRCodeStyleFormProps> = ({ mergeModuleDataProps:
   }, [left, top, width, height, form]);
 
   return (
-    <Form form={form} {...FormItemLayout}>
+    <Form form={form} {...FormItemLayout} onValuesChange={onChange}>
       <PositionForm prefixFormName={[moduleType]} />
     </Form>
   );
